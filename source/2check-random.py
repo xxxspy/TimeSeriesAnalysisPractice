8218e39e3838ad65008aa9381ec40a2aac38030d
孙亚东 <xxxspy@126.com>
2017-10-26 17:55:52 +0800

    add visualize_random_series_data

---
+++ source/2check-random.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import os 
+import random 
+import matplotlib.pyplot as plt
+
+#设置中文字体
+plt.rcParams['font.sans-serif']=['SimHei']
+
+DATA_DIR='../data/sample/'
+#固定随机数， 以便每次生成相同的随机数
+random.seed(10)
+def draw_random_data():
+    dpath = os.path.join(DATA_DIR, '附录1.1.xls')
+    data=pd.read_excel(dpath)
+    for i in range(data.shape[0]):
+        data.iloc[i, 1]=random.randint(20, 200)
+
+    print(data)
+    fig, ax = plt.subplots()
+    ax.plot(data['年份'], data['黑子数'])
+    ax.set(xlabel='年份', ylabel='黑子数', title='随机生成的黑子数')
+
+    plt.show()
+
+
+
+if __name__=='__main__':
+    draw_random_data()
