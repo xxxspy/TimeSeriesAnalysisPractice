5135ce029721f873eb53a9f7ec166ba6c77559ca
孙亚东 <xxxspy@126.com>
2017-10-26 18:03:35 +0800

    sequece_chart

--- source/2check-random.py
+++ source/2check-random.py
@@ -9,7 +9,7 @@ plt.rcParams['font.sans-serif']=['SimHei']
 DATA_DIR='../data/sample/'
 #固定随机数， 以便每次生成相同的随机数
 random.seed(10)
-def draw_random_data():
+def random_sequence_chart():
     dpath = os.path.join(DATA_DIR, '附录1.1.xls')
     data=pd.read_excel(dpath)
     for i in range(data.shape[0]):
@@ -19,10 +19,18 @@ def draw_random_data():
     fig, ax = plt.subplots()
     ax.plot(data['年份'], data['黑子数'])
     ax.set(xlabel='年份', ylabel='黑子数', title='随机生成的黑子数')
+    plt.savefig('../imgs/random-sunspots.png')
 
     plt.show()
 
-
+def sequence_chart():
+    dpath = os.path.join(DATA_DIR, '附录1.1.xls')
+    data=pd.read_excel(dpath)
+    fig, ax = plt.subplots()
+    ax.plot(data['年份'], data['黑子数'])
+    ax.set(xlabel='年份', ylabel='黑子数', title='黑子数')
+    plt.savefig('../imgs/sunspots.png')
+    plt.show()
 
 if __name__=='__main__':
-    draw_random_data()
+    random_sequence_chart()
