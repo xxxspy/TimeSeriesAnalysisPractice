0b44d5ca2f63096edc65b505d6efd18c72fb4686
孙亚东 <xxxspy@126.com>
2017-10-27 12:34:33 +0800

    add boxpierce_test

--- source/2check-random.py
+++ source/2check-random.py
@@ -51,5 +51,38 @@ def autocorr():
     plt.savefig('../imgs/autocorr.png')
     plt.show()
 
+def boxpierce_test():
+    '''计算box pierce 和 box ljung统计量'''
+    from statsmodels.sandbox.stats.diagnostic import acorr_ljungbox
+    dpath = os.path.join(DATA_DIR, '附录1.1.xls')
+    data=pd.read_excel(dpath)
+    x=data['黑子数']
+    qljungbox, pval, qboxpierce, pvalbp=acorr_ljungbox(x, boxpierce=True)
+    for i in range(len(pval)):
+        print('真实数据：qljungbox, pval, qboxpierce, pvalbp:',qljungbox[i], pval[i], qboxpierce[i], pvalbp[i])
+    fig, axes = plt.subplots(2,2)
+    axes[0,0].plot(qljungbox, label='qljungbox');axes[0,0].set_ylabel('真实-Q')
+    axes[0,0].plot(qboxpierce, label='qboxpierce')
+
+    axes[0,1].plot(pval, label='pval');axes[0,1].set_ylabel('P')
+    axes[0,1].plot(pvalbp, label='pvalbp')
+
+    x=[random.randint(1, 200) for i in range(data.shape[0])]
+    qljungbox, pval, qboxpierce, pvalbp=acorr_ljungbox(x, boxpierce=True)
+    axes[1,0].plot(qljungbox, label='qljungbox');axes[1,0].set_ylabel('随机-Q')
+    axes[1,0].plot(qboxpierce, label='qboxpierce')
+
+    axes[1,1].plot(pval, label='pval');axes[1,1].set_ylabel('P')
+    axes[1,1].plot(pvalbp, label='pvalbp')
+    axes[0,0].legend()
+    axes[0,1].legend()
+    axes[1,0].legend()
+    axes[1,1].legend()
+    plt.savefig('../imgs/boxpierce_test.png')
+    plt.show()
+    print('随机数据：qljungbox, pval, qboxpierce, pvalbp:',qljungbox, pval, qboxpierce, pvalbp)
+
+
+
 if __name__=='__main__':
-    autocorr()
+    boxpierce_test()
